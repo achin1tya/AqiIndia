b2d4f5b6cfdfbf201fc914df9eae382c272c3fb0
Achintya <achin1tya@gmail.com>
2021-07-18 22:08:09 +0530

    Added base file structure

---
+++ Readme.md
@@ -0,0 +1,13 @@
+This Project uses references from "https://github.com/bbc/vj-code-tests/tree/master/airquality"
+
+Using the web application 
+1. Install the /dist file in your machine and open ./dist/index.html in your browser . 
+
+To deploy the application for production 
+1. We can use Docker compose to set up the application or Set it up in s3 as discussed in the above repository . 
+
+Installations Required 
+1. Install node from https://nodejs.org/en/download/
+2. run npm install 
+3. open the index.html in your browser 
+
---
+++ index.js
@@ -0,0 +1,238 @@
+
+// placing data in js folder only to allow js to access it without cors issue in browsers.
+let data = {
+    "hero_1_image": "https://news.files.bbci.co.uk/vj/live/idt-images/image-slider-hello/SMOG_vdyw4.jpg",
+    "hero_1_title": "Delhi smog: How many cigarettes did you smoke this week?",
+    "article-info_1_byline": "By me!",
+    "article-info_1_date": "20 November 2017",
+    "article-info_1_category": "India",
+    "article-info_1_category_url": "http://www.bbc.co.uk/news/world/asia/india",
+    "p_1_value": "Breathing in Delhi is close to smoking more than a dozen cigarettes a day on an average. Click on your city to find out how many cigarettes you are smoking",
+    "total_cities_1_value": "32",
+    "p_2_value": "On November 7, Indian capital, Delhi woke up to a blanket of thick grey smog.  The toxic haze was so severe that politicians announced plans to close schools.  United Airlines shut all of its flights from the US to Delhi. Visibility was poor as pollution levels reached 30 times the World Health Organization's recommended limit in some areas.",
+    "p_3_value": "According to the data from the System of Air Quality Weather Forecasting and Research website, the levels of tiny particulate matter (known as PM 2.5) that enter deep into the lungs reached as high as 700 micrograms per cubic metre in some areas of Delhi on November 6th. The air quality standard for PM 2.5 is 15 µg/m3 and anything above 81 µg/m3 falls under poor air quality standard.",
+    "p_4_value": "Some reports claimed that New Delhi's air quality was so severe it was equivalent to smoking 45 cigarettes a day. We looked at the data to see if this was true. ",
+    "p_5_value": "Click on your city to find out how many cigarettes you have effectively smoked because of last week's pollution.",
+    "compare-tabs_1_method": "1 cigarette is equivalent to an air pollution of 22 μg/m3 (Absolute value: 21.6 μg/m3) for one day.",
+    "compare-tabs_1_title": "Select your city",
+    "compare-tabs_1_city_1_name": "Ghaziabad",
+    "compare-tabs_1_city_1_aqi": "283 PM2.5",
+    "compare-tabs_1_city_1_cigg": "12",
+    "compare-tabs_1_city_2_name": "Noida",
+    "compare-tabs_1_city_2_aqi": "213 PM2.5",
+    "compare-tabs_1_city_2_cigg": "10",
+    "compare-tabs_1_city_3_name": "Delhi",
+    "compare-tabs_1_city_3_aqi": "235 PM2.5",
+    "compare-tabs_1_city_3_cigg": "10",
+    "compare-tabs_1_city_4_name": "Lucknow",
+    "compare-tabs_1_city_4_aqi": "158 PM2.5",
+    "compare-tabs_1_city_4_cigg": "7",
+    "compare-tabs_1_city_5_name": "Gurugram",
+    "compare-tabs_1_city_5_aqi": "254 PM2.5",
+    "compare-tabs_1_city_5_cigg": "11",
+    "compare-tabs_1_city_6_name": "Faridabad",
+    "compare-tabs_1_city_6_aqi": "220 PM2.5",
+    "compare-tabs_1_city_6_cigg": "10",
+    "compare-tabs_1_city_7_name": "Varanasi",
+    "compare-tabs_1_city_7_aqi": "122 PM2.5",
+    "compare-tabs_1_city_7_cigg": "5",
+    "compare-tabs_1_city_8_name": "Kanpur",
+    "compare-tabs_1_city_8_aqi": "268 PM2.5",
+    "compare-tabs_1_city_8_cigg": "12",
+    "compare-tabs_1_city_9_name": "Patna",
+    "compare-tabs_1_city_9_aqi": "133 PM2.5",
+    "compare-tabs_1_city_9_cigg": "6",
+    "compare-tabs_1_city_10_name": "Muzaffarpur",
+    "compare-tabs_1_city_10_aqi": "114 PM2.5",
+    "compare-tabs_1_city_10_cigg": "5",
+    "compare-tabs_1_city_11_name": "Mandi Gobindgarh",
+    "compare-tabs_1_city_11_aqi": "75 PM2.5",
+    "compare-tabs_1_city_11_cigg": "3",
+    "compare-tabs_1_city_12_name": "Ludhiana",
+    "compare-tabs_1_city_12_aqi": "69 PM2.5",
+    "compare-tabs_1_city_12_cigg": "3",
+    "compare-tabs_1_city_13_name": "Jodhpur",
+    "compare-tabs_1_city_13_aqi": "126 PM2.5",
+    "compare-tabs_1_city_13_cigg": "6",
+    "compare-tabs_1_city_14_name": "Amritsar",
+    "compare-tabs_1_city_14_aqi": "61 PM2.5",
+    "compare-tabs_1_city_14_cigg": "3",
+    "compare-tabs_1_city_15_name": "Ahmedabad",
+    "compare-tabs_1_city_15_aqi": "91 PM2.5",
+    "compare-tabs_1_city_15_cigg": "4",
+    "compare-tabs_1_city_16_name": "Panchkula",
+    "compare-tabs_1_city_16_aqi": "41 PM2.5",
+    "compare-tabs_1_city_16_cigg": "2",
+    "compare-tabs_1_city_17_name": "Gaya",
+    "compare-tabs_1_city_17_aqi": "70 PM2.5",
+    "compare-tabs_1_city_17_cigg": "3",
+    "compare-tabs_1_city_18_name": "Nagpur",
+    "compare-tabs_1_city_18_aqi": "27 PM2.5",
+    "compare-tabs_1_city_18_cigg": "1",
+    "compare-tabs_1_city_19_name": "Vijaywada",
+    "compare-tabs_1_city_19_aqi": "32 PM2.5",
+    "compare-tabs_1_city_19_cigg": "1",
+    "compare-tabs_1_city_20_name": "Chandrapur",
+    "compare-tabs_1_city_20_aqi": "43 PM2.5",
+    "compare-tabs_1_city_20_cigg": "1",
+    "compare-tabs_1_city_21_name": "Chennai",
+    "compare-tabs_1_city_21_aqi": "58 PM2.5",
+    "compare-tabs_1_city_21_cigg": "2",
+    "compare-tabs_1_city_22_name": "Bhiwadi",
+    "compare-tabs_1_city_22_aqi": "103 PM2.5",
+    "compare-tabs_1_city_22_cigg": "4",
+    "compare-tabs_1_city_23_name": "Mumbai",
+    "compare-tabs_1_city_23_aqi": "44 PM2.5",
+    "compare-tabs_1_city_23_cigg": "2",
+    "compare-tabs_1_city_24_name": "Nashik",
+    "compare-tabs_1_city_24_aqi": "51 PM2.5",
+    "compare-tabs_1_city_24_cigg": "2",
+    "compare-tabs_1_city_25_name": "Thane",
+    "compare-tabs_1_city_25_aqi": "41 PM2.5",
+    "compare-tabs_1_city_25_cigg": "2",
+    "compare-tabs_1_city_26_name": "Aurangabad",
+    "compare-tabs_1_city_26_aqi": "48 PM2.5",
+    "compare-tabs_1_city_26_cigg": "2",
+    "compare-tabs_1_city_27_name": "Hyderabad",
+    "compare-tabs_1_city_27_aqi": "56 PM2.5",
+    "compare-tabs_1_city_27_cigg": "2",
+    "compare-tabs_1_city_28_name": "Visakhapatnam",
+    "compare-tabs_1_city_28_aqi": "51 PM2.5",
+    "compare-tabs_1_city_28_cigg": "2",
+    "compare-tabs_1_city_29_name": "Rajamahandevaram",
+    "compare-tabs_1_city_29_aqi": "44 PM2.5",
+    "compare-tabs_1_city_29_cigg": "2",
+    "compare-tabs_1_city_30_name": "Chittoor",
+    "compare-tabs_1_city_30_aqi": "11 PM2.5",
+    "compare-tabs_1_city_30_cigg": "0",
+    "compare-tabs_1_city_31_name": "Bangalore\n",
+    "compare-tabs_1_city_31_aqi": "23 PM2.5",
+    "compare-tabs_1_city_31_cigg": "1",
+    "compare-tabs_1_city_32_name": "Trivandrum",
+    "compare-tabs_1_city_32_aqi": "53 PM2.5",
+    "compare-tabs_1_city_32_cigg": "2",
+    "p_6_value": "Methodology for calculation: ",
+    "p_7_value": "A report by Berkeley Earth showed that in China, 1.6 million people die every year from an average exposure of 52 μg/m3 of PM2.5. To kill 1.6 million people would require, assuming 1.36 x9^6 deaths per cigarette, 1.1 trillion cigarettes. Since the population of China is 1.35 billion, that comes to 764 cigarettes every year per person, or about 2.4 cigarettes per day.",
+    "p_8_value": "Put another way, one cigarette is equivalent to an air pollution of 22 μg/m3 (Absolute value: 21.6 μg/m3) for one day.",
+    "p_9_value": "The average PM2.5 in Beijing over the year is about 85 μg/m3, equivalent to about 4 cigarettes per day. Similarly, if the air pollution in New Delhi, India, is 546 μg/m3, it is equivalent to about 25 cigarettes each day.",
+    "p_10_value": "We took the PM 2.5 data of 33 cities in India every day for one week. We calculated the mean of 6 days PM 2.5 data of each city and divided it by 21.6 μg/m3 to get the number of cigarettes. It is also imperative to say the numbers shown is the assumption that you ideally spent most of your time breathing air from outside."
+}
+var keyNames = Object.keys(data);
+
+
+function autocomplete(currentElement, arr) {
+    /*the autocomplete function takes two arguments,
+    the text field element and an array of possible autocompleted values:*/
+    let currentFocus;
+    /*execute a function when someone writes in the text field:*/
+    currentElement.addEventListener("input", function(e) {
+        let a, b, i, val = this.value;
+        closeAllLists();
+        if (!val) { return false;}
+        currentFocus = -1;
+        suggestionArray = document.createElement("DIV");
+        suggestionArray.setAttribute("id", this.id + "autocomplete-list");
+        suggestionArray.setAttribute("class", "autocomplete-items");
+        /*append the DIV element as a child of the autocomplete container:*/
+        this.parentNode.appendChild(suggestionArray);
+        for (i = 0; i < arr.length; i++) {
+          /*check if the item starts with the same letters as the text field value:*/
+          if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+            suggestion = document.createElement("DIV");
+            suggestion.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+            suggestion.innerHTML += arr[i].substr(val.length);
+            suggestion.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+            /*execute a function when someone clicks on the item value (DIV element):*/
+            suggestion.addEventListener("click", function(e) {
+                currentElement.value = this.getElementsByTagName("input")[0].value;
+                closeAllLists();
+            });
+            suggestionArray.appendChild(suggestion);
+          }
+        }
+    });
+    /*execute a function presses a key on the keyboard:*/
+    currentElement.addEventListener("keydown", function(e) {
+        let currentActiveElement = document.getElementById(this.id + "autocomplete-list");
+        if (currentActiveElement) currentActiveElement = currentActiveElement.getElementsByTagName("div");
+        if (e.keyCode == 40) {
+          //If the arrow DOWN key is pressed
+          currentFocus++;
+          addActive(currentActiveElement);
+        } else if (e.keyCode == 38) { //up
+          //If the arrow UP key is pressed
+          currentFocus--;
+          addActive(currentActiveElement);
+        } else if (e.keyCode == 13) {
+          /*If the ENTER key is pressed, prevent the form from being submitted,*/
+          e.preventDefault();
+          if (currentFocus > -1) {
+            if (currentActiveElement) currentActiveElement[currentFocus].click();
+          }
+        }
+    });
+    function addActive(x) {
+      /*a function to classify an item as "active":*/
+      if (!x) return false;
+      removeActive(x);
+      if (currentFocus >= x.length) currentFocus = 0;
+      if (currentFocus < 0) currentFocus = (x.length - 1);
+      x[currentFocus].classList.add("autocomplete-active");
+    }
+    function removeActive(x) {
+      /*a function to remove the "active" class from all autocomplete items:*/
+      for (var i = 0; i < x.length; i++) {
+        x[i].classList.remove("autocomplete-active");
+      }
+    }
+    function closeAllLists(elmnt) {
+      /*close all autocomplete lists in the document,
+      except the one passed as an argument:*/
+      var x = document.getElementsByClassName("autocomplete-items");
+      for (var i = 0; i < x.length; i++) {
+        if (elmnt != x[i] && elmnt != currentElement) {
+          x[i].parentNode.removeChild(x[i]);
+        }
+      }
+    }
+    /*execute a function when someone clicks in the document:*/
+    document.addEventListener("click", function (e) {
+        closeAllLists(e.target);
+    });
+  }
+  
+  /*An array containing all the state names in India for which we have data :*/
+  var countries = ["Ghaziabad","Noida","Delhi","Lucknow","Gurugram","Faridabad","Varanasi","Kanpur","Patna","Muzaffarpur","Mandi Gobindgarh","Ludhiana","Jodhpur","Amritsar","Ahmedabad","Panchkula","Gaya","Nagpur","Vijaywada","Chandrapur","Chennai","Mumbai","Nashik","Thane","Aurangabad","Hyderabad","Visakhapatnam","Rajamahandevaram","Chittoor","Bangalore","Trivandrum"];
+  
+  /*initiate the autocomplete function on the "userInput" element, and pass along the countries array as possible autocomplete values:*/
+  autocomplete(document.getElementById("userInput"), countries);
+
+  function searchTheAQI(){
+        let cityName = document.getElementById("userInput").value;
+        getCityData(cityName);
+  }
+
+  function getCityData(cityName){
+    // Note to make this realtime , we should be fetching this from a backend route/api 
+    // as data is constant for now so included directly here and avoided CORS 
+
+    // traverse the data 
+    let aqi = "";
+    let noOfCigg = "";
+    for (let i = 0 ;i< keyNames.length;i++){
+        // check if currentvalue is the city searched
+        if(data[keyNames[i]].toLowerCase() == cityName.toLowerCase()){
+            // if its a match , then fill the aqi and no of ciggarets 
+            aqi = data[keyNames[i+1]];
+            noOfCigg = data[keyNames[i+2]];
+        }
+    }
+    console.log(aqi + " ",noOfCigg);
+    displayData(aqi , noOfCigg);
+  }
+
+  function displayData(aqi , noOfCigg){
+      // move the search-box to new poistion 
+      document.getElementById("aqi_val").innerHTML = aqi;
+      document.getElementById("cigarette_val").innerHTML = noOfCigg;
+  }
